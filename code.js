02fac0bcfc3b26f0aa6948205dbc157533d57644
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2023-02-03 00:14:08 +0530

    first

---
+++ code.js
@@ -0,0 +1,192 @@
+module.exports={
+    answers:[``,`#include <cmath>
+    #include <cstdio>
+    #include <vector>
+    #include <iostream>
+    #include <algorithm>
+    using namespace std;
+    
+    int solveMeFirst(int a, int b) {
+     
+      return a+b;
+    }
+    
+    int main() {
+      int num1, num2;
+      int sum;
+      cin>>num1>>num2;
+      sum = solveMeFirst(num1,num2);
+      cout<<sum;
+      return 0;
+    }`,
+
+
+    `
+    #include <bits/stdc++.h>
+    using namespace std;
+    
+    int main(){
+       int number_of_elements;
+       cin>>number_of_elements;
+       vector <int> array(number_of_elements);
+       int sum_of_array=0;
+    
+       for(int i=0;i<number_of_elements;i++){
+             cin>>array[i];
+           sum_of_array+=array[i];
+        }
+      cout<<sum_of_array;
+      return 0;
+    
+      }
+      `,
+
+      `
+      #include <cmath>
+      #include <cstdio>
+      #include <cstdlib>
+      #include <iostream>
+     
+      using namespace std;
+      
+      int main()
+      {
+          int a0;
+          int a1;
+          int a2;
+          cin >> a0 >> a1 >> a2;
+          int b0;
+          int b1;
+          int b2;
+          cin >> b0 >> b1 >> b2;
+          int a=0,b=0;
+          if(a0>b0)
+              a++;
+          if(a0<b0)
+              b++;
+          if(a1>b1)
+              a++;
+          if(a1<b1)
+              b++;
+          if(a2>b2)
+               a++;
+          if(a2<b2)
+              b++;
+          cout<<a<<" "<<b;
+          return 0;
+      }
+      
+      `,  
+   
+      
+     `
+      #include <cmath>
+      #include <cstdio>
+      #include <vector>
+      #include <iostream>
+      #include <algorithm>
+      using namespace std;
+      
+      int main()
+      {
+      int n;
+      long long int sum=0;
+      
+      cin >> n;
+      
+      vector<int> arr(n);
+      
+      for(int arr_i = 0;arr_i < n;arr_i++)
+      {
+      cin >> arr[arr_i];
+      sum+=arr[arr_i];
+      }
+      
+      cout<<sum;
+      return 0;
+      
+      }`,
+
+
+
+        `#include <math.h>
+        #include <stdio.h>
+        #include <string.h>
+        #include <stdlib.h>
+        #include <assert.h>
+        #include <limits.h>
+        #include <stdbool.h>
+        int main()
+        {
+         
+            int n, j;
+            int i=0,RightDiagonalSum=0,LeftDiagonalSum=0, firstarray, secondarray;
+            
+         scanf("%d",&n);
+            int a[n][n];
+            
+         for(int firstarray = 0; firstarray < n; firstarray++)
+         {
+               for(int secondarray = 0; secondarray < n; secondarray++)
+            {
+                 
+                  scanf("%d",&a[firstarray][secondarray]);
+               }
+            }
+         
+          while(i<n)
+          {
+            RightDiagonalSum=RightDiagonalSum+a[i][i];
+            i++;
+          }
+            
+         j=n-1,i=0;
+            
+          while(i<n)
+          {
+            LeftDiagonalSum=LeftDiagonalSum+a[i][j];
+            i++;
+            j--;
+          }
+            
+         printf("%d",abs(RightDiagonalSum-LeftDiagonalSum));
+            return 0;
+        }`,
+            
+            `#include <bits/stdc++.h>
+            using namespace std;
+            
+            int main()
+             {
+                int n,c1=0,c2=0,c3=0;
+                cin >> n;
+                
+             vector<int> arr(n);
+                
+             for(int arr_i = 0;arr_i < n;arr_i++)
+             {
+                   cin >> arr[arr_i];
+                    
+             if(arr[arr_i]>0)
+                    {
+                     c1++;
+             }    
+                    if(arr[arr_i]<0)
+                    {
+                     c2++;
+                 }
+                    if(arr[arr_i]==0)
+                    {
+                     c3++;
+                    }
+                    cout<<setprecision(6)<<(float)c1/n<<endl;
+                    cout<<setprecision(6)<<(float)c2/n<<endl;
+                    cout<<setprecision(6)<<(float)c3/n<<endl;
+                return 0;
+            }
+            
+            
+            `
+     ]
+  }
+  
---
+++ hkasync.js
@@ -0,0 +1,73 @@
+const puppeteer=require("puppeteer");
+const promise=require("promise")
+const url="https://www.hackerrank.com/auth/login"
+const email="sinhashalini708@gmail.com";
+const pass="shalini"
+const code=require("./code")
+let page;
+
+(async function hackerrank(){
+    const browserOpen= await puppeteer.launch({headless:false,args:["--start-maximized"],defaultViewport:null})
+    let browserarr= await browserOpen.pages()
+     page=await browserarr[0];
+     await page.goto(url);
+    await waitAndclick('input[id="input-1"]',page);
+    await page.type('input[id="input-1"]',email);
+    await page.type('input[id="input-2"]',pass);
+    await waitAndclick("button[class='ui-btn ui-btn-large ui-btn-primary auth-button ui-btn-styled']",page);
+    await waitAndclick('div[class="topic-name"]',page)
+    await waitAndclick('input[value="warmup"]',page)
+  
+const elementHandles = await page.$$("a[class='js-track-click challenge-list-item']");
+const propertyJsHandles = await Promise.all(
+  elementHandles.map(handle => handle.getProperty('href'))
+);
+const hrefs = await Promise.all(
+  propertyJsHandles.map(handle => handle.jsonValue())
+);
+
+
+for(let i=1;i<5;i++){
+  await questionSolver(hrefs[i],page,code.answers[i])
+}
+
+   
+      
+})()
+
+ async function questionSolver(question,page,answer){
+    await page.goto(question,{delay:5000})
+    await waitAndclick('div[class="monaco-scrollable-element editor-scrollable vs"]',page)
+    await page.click("input[class='checkbox-input']")
+    await page.type("textarea[class='input text-area custominput auto-width']",answer)
+    await page.keyboard.down("Control")
+    await page.keyboard.press("A")
+    await page.keyboard.press("X")
+    await page.keyboard.up("Control")
+    await waitAndclick('div[class="monaco-scrollable-element editor-scrollable vs"]',page)
+    await page.keyboard.down("Control")
+    await page.keyboard.press("A")
+    await page.keyboard.press("V")
+    await page.keyboard.up("Control")
+    await page.click("button[class='ui-btn ui-btn-normal ui-btn-primary pull-right hr-monaco-submit ui-btn-styled']")
+    await page.evaluate(async() => {
+      setTimeout(function(){
+          console.log('waiting');
+      }, 20000)
+    });
+    
+      
+ }
+
+ async function waitAndclick(selector,page){
+   try{
+     await page.waitForSelector(selector,{visible:true}) 
+     await page.click(selector)
+    
+   }
+   catch(err){
+     console.log(err);
+   }      
+         
+        
+   }
